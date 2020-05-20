431740f582c76b08909d8b29404eddf4f05db556
AlexGruPerm <ugr@bk.ru>
2020-05-21 02:43:11 +0500

    Control commit before adding new case classes for store statistics by ts.

--- src/main/scala/application/StatHelper.scala
+++ src/main/scala/application/StatHelper.scala
@@ -1,10 +1,8 @@
 package application
 
-import db.Ucp.UcpZLayer
 import env.CacheObject.CacheManager
 import env.EnvContainer.ZEnvConfLogCache
-import stat.StatObject.WsStat
-import zio.{Task, ZIO}
+import zio.ZIO
 import zio.logging.log
 
 object StatHelper {
@@ -15,15 +13,11 @@ object StatHelper {
       startTs <- cache.getWsStartTs
       gc <- cache.getGetCount
       cc <- cache.getCleanCount
-      _ <- log.info(s"~~~~~~~~~~ WS CURRENT STATISTIC ~~~~~~~~~~~~~~~~~")
-      _ <- log.info(s"uptime : ${(System.currentTimeMillis - startTs)/1000} sec. Get(count)=${gc.size}")
+      _ <- log.trace(s"~~~~~~~~~~ WS CURRENT STATISTIC ~~~~~~~~~~~~~~~~~")
+      _ <- log.trace(s"uptime : ${(System.currentTimeMillis - startTs)/1000} sec. Get(count)=${gc.size}")
       _ <- cache.clearGetCounter
-      //todo: remove on
-      _ <- ZIO.foreach(gc.toList)(elm => log.info(s" 1 ELM : ${elm.ts} - ${elm.cnt}"))
-      _ <- ZIO.succeed(gc.foreach(elm => log.info(s"   2 ELM : ${elm.ts} - ${elm.cnt}")))
-
-      _ <- ZIO.foreach(cc.toList)(elm => log.info(s" CLEAN STAT : ${elm.ts} - ${elm.cnt}"))
-
+      _ <- ZIO.foreach(gc.toList)(elm => log.trace(s" GET STAT: ${elm.ts} - ${elm.cnt}"))
+      _ <- ZIO.foreach(cc.toList)(elm => log.trace(s" CLEAN STAT : ${elm.ts} - ${elm.cnt}"))
       _ <- log.info(s"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     } yield ()
 
